07a388d0705456187b95500b80f6c1fdf0514b06
Alex Marchant <alexjmarchant@gmail.com>
2017-12-01 05:54:10 -0500

    Split up the code a bit

---
+++ src/lexer.rs
@@ -0,0 +1,62 @@
+#[derive(Debug)]
+pub enum Token {
+    OpenBrace,
+    CloseBrace,
+    OpenParen,
+    CloseParen,
+    Semicolon,
+    KeywordInt,
+    KeywordReturn,
+    IntegerLiteral(i64),
+    Identifier(String),
+}
+
+pub fn parse_tokens(contents: String) -> Vec<Token> {
+    let token_strings: Vec<String> = parse_token_strings(contents);
+    let mut tokens = Vec::new();
+    for token_string in &token_strings {
+        match token_string.as_ref() {
+            "{" => tokens.push(Token::OpenBrace),
+            "}" => tokens.push(Token::CloseBrace),
+            "(" => tokens.push(Token::OpenParen),
+            ")" => tokens.push(Token::CloseParen),
+            ";" => tokens.push(Token::Semicolon),
+            "int" => tokens.push(Token::KeywordInt),
+            "return" => tokens.push(Token::KeywordReturn),
+            _ => {
+                match token_string.parse::<i64>() {
+                    Ok(i) => tokens.push(Token::IntegerLiteral(i)),
+                    Err(_) => tokens.push(Token::Identifier(token_string.clone())),
+                }
+            },
+        }
+    }
+    return tokens;
+}
+
+fn parse_token_strings(contents: String) -> Vec<String> {
+    let mut tokens = Vec::new();
+    let mut current_token = String::new();
+    for char in contents.chars() { 
+        match char {
+            ' ' | '\n' => {
+                if current_token.len() > 0 {
+                    tokens.push(current_token);
+                    current_token = String::new();
+                }
+            },
+            '{' | '}' | '(' | ')' | ';' => {
+                if current_token.len() > 0 {
+                    tokens.push(current_token);
+                    current_token = String::new();
+                }
+                let mut token = String::new();
+                token.push(char);
+                tokens.push(token);
+            },
+            _ => current_token.push(char),
+        }
+    }
+    return tokens;
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,8 @@
+mod lexer;
+
 use std::env;
 use std::io::prelude::*;
 use std::fs::File;
-
-#[derive(Debug)]
-enum Token {
-    OpenBrace,
-    CloseBrace,
-    OpenParen,
-    CloseParen,
-    Semicolon,
-    KeywordInt,
-    KeywordReturn,
-    IntegerLiteral(i64),
-    Identifier(String),
-}
 
 fn main() {
     let command = env::args().nth(1).expect("Missing argument");
@@ -26,7 +15,7 @@ fn main() {
 fn lexer() {
     let file_name = env::args().nth(2).expect("Missing argument");
     let contents = read_file(file_name);
-    let tokens = parse_tokens(contents);
+    let tokens = lexer::parse_tokens(contents);
     println!("{:?}", tokens);
 }
 
@@ -35,54 +24,5 @@ fn read_file(file_name: String) -> String {
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("Unable to read the file");
     return contents;
-}
-
-fn parse_tokens(contents: String) -> Vec<Token> {
-    let token_strings: Vec<String> = parse_token_strings(contents);
-    let mut tokens = Vec::new();
-    for token_string in &token_strings {
-        match token_string.as_ref() {
-            "{" => tokens.push(Token::OpenBrace),
-            "}" => tokens.push(Token::CloseBrace),
-            "(" => tokens.push(Token::OpenParen),
-            ")" => tokens.push(Token::CloseParen),
-            ";" => tokens.push(Token::Semicolon),
-            "int" => tokens.push(Token::KeywordInt),
-            "return" => tokens.push(Token::KeywordReturn),
-            _ => {
-                match token_string.parse::<i64>() {
-                    Ok(i) => tokens.push(Token::IntegerLiteral(i)),
-                    Err(_) => tokens.push(Token::Identifier(token_string.clone())),
-                }
-            },
-        }
-    }
-    return tokens;
-}
-
-fn parse_token_strings(contents: String) -> Vec<String> {
-    let mut tokens = Vec::new();
-    let mut current_token = String::new();
-    for char in contents.chars() { 
-        match char {
-            ' ' | '\n' => {
-                if current_token.len() > 0 {
-                    tokens.push(current_token);
-                    current_token = String::new();
-                }
-            },
-            '{' | '}' | '(' | ')' | ';' => {
-                if current_token.len() > 0 {
-                    tokens.push(current_token);
-                    current_token = String::new();
-                }
-                let mut token = String::new();
-                token.push(char);
-                tokens.push(token);
-            },
-            _ => current_token.push(char),
-        }
-    }
-    return tokens;
 }
 
